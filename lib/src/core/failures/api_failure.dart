c934f56fe0baf931a56c0d3e60ddb9e147f46ee6
João Paulo Camargo <joaopaulobrasiliano@gmail.com>
2024-10-21 09:33:02 -0300

    feat: update ApiFailure error

--- lib/src/core/failures/api_failure.dart
+++ lib/src/core/failures/api_failure.dart
@@ -1,3 +1,10 @@
-abstract interface class ApiFailure implements Exception {
+abstract interface class Failure implements Exception {
   String? get message;
+}
+
+class ApiFailure extends Failure {
+  ApiFailure(this.message);
+
+  @override
+  final String? message;
 }
